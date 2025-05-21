5a8687b55e76c38479122a2f8aba6900b79e6c73
Mathias C. Cronjäger <7360873+Cronjaeger@users.noreply.github.com>
2025-05-21 09:57:27 +0200

    Update app.py
    Run on 0.0.0.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import Levenshtein
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def index():
         return render_template("index.html", title=title, real_paragraph=paragraph)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
